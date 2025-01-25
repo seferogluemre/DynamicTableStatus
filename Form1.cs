6e0a0d7f84b889bc8213558fccec688ec6aef5c9
xEmre1-53 <157003672+xEmre1-53@users.noreply.github.com>
2025-01-26 02:27:27 +0300

    Creating buttons dynamically on the form

--- Form1.cs
+++ Form1.cs
@@ -16,5 +16,25 @@ namespace DynamicTableStatus
         {
             InitializeComponent();
         }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            int buttonWidth = 100;
+            int buttonHeight = 50;
+            int padding = 10;
+            int xOffset = 10;
+            int yOffset = 10;
+
+
+            for(int i = 0; i <= 12; i++)
+            {
+                Button button = new Button();
+                button.Text = $"Masa{i+1}";
+                button.Size=new Size(buttonWidth, buttonHeight);
+                button.Location=new Point(xOffset+(i%4)*(buttonWidth+padding),
+                    yOffset + (i / 4) * (buttonHeight + padding));
+                this.Controls.Add(button);
+            }
+        }
     }
 }
