247dd6f027f7a7908367546b0f496a58c628c4ab
xEmre1-53 <157003672+xEmre1-53@users.noreply.github.com>
2025-01-26 02:47:19 +0300

    Following the form according to real-time table developments and ensuring a dynamic structure

--- Form1.cs
+++ Form1.cs
@@ -1,11 +1,6 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
 using System.Drawing;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows.Forms;
 
 namespace DynamicTableStatus
@@ -17,10 +12,24 @@ namespace DynamicTableStatus
             InitializeComponent();
         }
 
-        DbTableStatusEntities context = new DbTableStatusEntities();
+      
 
         private void Form1_Load(object sender, EventArgs e)
         {
+            timer1.Start();
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            DbTableStatusEntities context = new DbTableStatusEntities();
+
+            var buttons = this.Controls.OfType<Button>().ToList();
+
+            foreach (var btn in buttons)
+            {
+                this.Controls.Remove(btn);
+            }
+
             var values = context.TblTables.ToList();
 
             int buttonWidth = 100;
@@ -30,13 +39,13 @@ namespace DynamicTableStatus
             int yOffset = 10;
 
 
-            for(int i = 0; i <values.Count; i++)
+            for (int i = 0; i < values.Count; i++)
             {
                 var item = values[i];
                 Button button = new Button();
                 button.Text = item.TableNumber.ToString();
-                button.Size=new Size(buttonWidth, buttonHeight);
-                button.Location=new Point(xOffset+(i%4)*(buttonWidth+padding),
+                button.Size = new Size(buttonWidth, buttonHeight);
+                button.Location = new Point(xOffset + (i % 4) * (buttonWidth + padding),
                     yOffset + (i / 4) * (buttonHeight + padding));
                 button.BackColor = (bool)item.Status ? System.Drawing.Color.Green : System.Drawing.Color.Red;
                 this.Controls.Add(button);
