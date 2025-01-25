44299594cbf256beba99143be4980e12d2b6b9d5
xEmre1-53 <157003672+xEmre1-53@users.noreply.github.com>
2025-01-26 02:35:17 +0300

    Giving color according to the current status of the table

--- Form1.cs
+++ Form1.cs
@@ -17,8 +17,12 @@ namespace DynamicTableStatus
             InitializeComponent();
         }
 
+        DbTableStatusEntities context = new DbTableStatusEntities();
+
         private void Form1_Load(object sender, EventArgs e)
         {
+            var values = context.TblTables.ToList();
+
             int buttonWidth = 100;
             int buttonHeight = 50;
             int padding = 10;
@@ -26,13 +30,15 @@ namespace DynamicTableStatus
             int yOffset = 10;
 
 
-            for(int i = 0; i <= 12; i++)
+            for(int i = 0; i <values.Count; i++)
             {
+                var item = values[i];
                 Button button = new Button();
-                button.Text = $"Masa{i+1}";
+                button.Text = item.TableNumber.ToString();
                 button.Size=new Size(buttonWidth, buttonHeight);
                 button.Location=new Point(xOffset+(i%4)*(buttonWidth+padding),
                     yOffset + (i / 4) * (buttonHeight + padding));
+                button.BackColor = (bool)item.Status ? System.Drawing.Color.Green : System.Drawing.Color.Red;
                 this.Controls.Add(button);
             }
         }
